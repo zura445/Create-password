44a4c2602c7f94073a3931850e92c4c3b0eb0e44
zura tetrashvili <zuratetra@gmail.com>
2025-05-15 20:56:00 +0400

    create monitor visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Monitor from "./components/Monitor";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="w-[540px]">
+        <Monitor />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Monitor.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Monitor() {
+  return (
+    <div>
+      <p className="text-gray-800 text-2xl text-center">Password Generator</p>
+      <div className="bg-black-700 flex justify-between items-center py-5.5 px-8 mt-6">
+        <div className="text-3xl text-white ">PTx1f5DaFX</div>
+        <div className="">
+          <img src="copy.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Monitor;
